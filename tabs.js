e04f0906b9d4d4f0be5f5690eeb362bc6e20a0bb
Sam-Robertson <robertsonnew@gmail.com>
2020-03-05 16:47:06 -0700

    Add files via upload
    This is a file

--- tabs.js
+++ tabs.js
@@ -1,4 +1,4 @@
-function openCity(evt, tabName) {
+function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
@@ -12,17 +12,3 @@ function openCity(evt, tabName) {
     evt.currentTarget.className += " active";
   }
 
-
-//   function openCity(evt, cityName) {
-//     var i, tabcontent, tablinks;
-//     tabcontent = document.getElementsByClassName("tabcontent");
-//     for (i = 0; i < tabcontent.length; i++) {
-//       tabcontent[i].style.display = "none";
-//     }
-//     tablinks = document.getElementsByClassName("tablinks");
-//     for (i = 0; i < tablinks.length; i++) {
-//       tablinks[i].className = tablinks[i].className.replace(" active", "");
-//     }
-//     document.getElementById(cityName).style.display = "block";
-//     evt.currentTarget.className += " active";
-//   }
